a9d154f6634097c8cd94578584600b76ffd8e09d
Maciej Malecki <maciej.malecki@capgemini.com>
2021-02-08 13:08:50 +0100

    Initial gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "mtshex"
+// -- domain circle
+include(":domain")
+// -- application circle
+include(":app")
+// -- infrastructure circle
+include(":infra:web")
+include(":infra:store")
